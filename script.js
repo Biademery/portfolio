16c93f8709b9e1a763ae8822930f59244ed06d9a
Bia D'Emery <biademery@gmail.com>
2022-12-30 15:59:00 -0300

    add js from html

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 const hamburger = document.querySelector('.hamburger');
 const navLink = document.querySelector('.nav__link');
+const myBtn = document.querySelector('#myBtn');
 
 hamburger.addEventListener('click', () => {
-  navLink.classList.toggle('hide');
+  navLink.style.display = 'block';
 });
 
-const mybutton = document.getElementById("myBtn");
+navLink.addEventListener('click', () => {
+  navLink.style.display = 'none';
+});
 
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
+window.onscroll = function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    myBtn.style.display = 'block';
   } else {
-    mybutton.style.display = "none";
+    myBtn.style.display = 'none';
   }
-}
+};
 
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+myBtn.addEventListener('click', topFunction);
+
+const date = document.querySelector('#date');
+
+date.textContent = `Copyright © ${new Date().getFullYear()}`;
