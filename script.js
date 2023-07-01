f038f9693fde5b0a029bc7dfc83dbfa870c45c9d
Bia D'Emery <biademery@gmail.com>
2023-07-01 16:26:38 -0300

    code update

--- script.js
+++ script.js
@@ -1,38 +1,12 @@
-const hamburger = document.querySelector('.hamburger');
-const navLink = document.querySelector('.nav__link');
-const navbar = document.querySelector('.navbar');
-const myBtn = document.querySelector('#myBtn');
-const date = document.querySelector('#date');
+// const myBtn = document.querySelector("#myBtn");
+const date = document.querySelector("#date");
 
-hamburger.addEventListener('click', () => {
-  if (navLink.style.display === 'none') {
-    navLink.style.display = 'block';
-  } else {
-    navLink.style.display = 'none';
-  }
-});
 
-navLink.addEventListener('click', () => {
-  navLink.style.display = 'none';
-});
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
 
-window.onscroll = function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    myBtn.style.display = 'block';
-    navbar.style.height = '10%';
-    navLink.style.top = '50px';
-  } else {
-    myBtn.style.display = 'none';
-    navbar.style.height = '14%';
-    navLink.style.top = '80px';
-  }
-};
-
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
-myBtn.addEventListener('click', topFunction);
+// myBtn.addEventListener("click", topFunction);
 
 date.textContent = `Copyright © ${new Date().getFullYear()}`;
